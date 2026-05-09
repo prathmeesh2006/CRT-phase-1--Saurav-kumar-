42044e69a89a5404c9c541ebfebc01c5dc950b8f
Prathmesh Tiwari <prathmeesh2006@gmail.com>
2026-05-09 16:03:18 +0530

    change

---
+++ Day 9/sum_of_array.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class sum
+{
+    int sum_of_array(int arr[], int n)
+    {
+        int sum = 0;
+        for(int i = 0; i < n; i++)
+        {
+            sum += arr[i];
+        }
+        return sum;
+    }       
+};
+int main()
+{
+    int arr[5]= {1, 2, 3, 4, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    sum s;
+    cout<<"Sum of array is: "<<arr<<endl;
+    return 0;
+}
